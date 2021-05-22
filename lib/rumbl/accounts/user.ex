6e78f4650807b1a17b53bdb3d1cb2b9cd9eaa819
Luu Gia Thuy <luugiathuy@gmail.com>
2021-05-22 11:26:43 +0800

    Add argon2_elixir lib. Add password to create user

--- lib/rumbl/accounts/user.ex
+++ lib/rumbl/accounts/user.ex
@@ -8,6 +8,8 @@ defmodule Rumbl.Accounts.User do
   schema "users" do
     field :name, :string
     field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
 
     timestamps()
   end
@@ -17,5 +19,22 @@ defmodule Rumbl.Accounts.User do
     |> cast(attrs, [:name, :username])
     |> validate_required([:name, :username])
     |> validate_length(:username, min: 1, max: 20)
+  end
+
+  def registration_changeset(user, params) do
+    user
+    |> changeset(params)
+    |> cast(params, [:password])
+    |> validate_required([:password])
+    |> validate_length(:password, min: 8)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Argon2.hash_pwd_salt(pass))
+      _ -> changeset
+    end
   end
 end
